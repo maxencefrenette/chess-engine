f42d8f0c0aa059bc22e5779e248b41cb3e7d7b0e
Maxence Frenette <maxence.frenette@gmail.com>
2025-02-17 09:51:08 -0800

    Add learnign rate tuning for pico class models

--- src/training/experiments/learning_rate.py
+++ src/training/experiments/learning_rate.py
@@ -20,20 +20,37 @@ def _(__file__):
 
 
 @app.cell
-def _(mo):
-    run_button = mo.ui.run_button(label="Run experiment")
-    run_button
-    return (run_button,)
+def _(mo, np):
+    configs = {
+        "debug": np.geomspace(0.00001, 0.1, num=30).tolist(),
+        "pico": np.geomspace(0.00001, 0.1, num=30).tolist(),
+    }
+
+    checkboxes = {config: mo.ui.checkbox() for config in configs.keys()}
+    dictionnary = mo.ui.dictionary(checkboxes)
+    run_button = mo.ui.run_button(label="Run experiments")
+
+    mo.vstack([dictionnary, run_button])
+    return checkboxes, configs, dictionnary, run_button
 
 
 @app.cell
-def _(CSVLogger, Path, __file__, mo, np, run_button, train, yaml):
-    def train_experiment(config_name: str):
+def _(
+    CSVLogger,
+    Path,
+    __file__,
+    configs,
+    dictionnary,
+    mo,
+    run_button,
+    train,
+    yaml,
+):
+    def train_experiment(config_name: str, learning_rates: list[float]):
         with open(Path(__file__).parents[1] / f"configs/{config_name}.yaml") as f:
             config = yaml.safe_load(f)
 
         base_lr = config["model"]["learning_rate"]
-        learning_rates = np.geomspace(0.00001, 0.1, num=30).tolist()
 
         for i, lr in enumerate(mo.status.progress_bar(learning_rates)):
             config_copy = config.copy()
@@ -50,12 +67,14 @@ def _(CSVLogger, Path, __file__, mo, np, run_button, train, yaml):
                     metrics = train(config_copy, csv_logger=csv_logger)
 
     if run_button.value:
-        train_experiment("debug")
-    return (train_experiment,)
+        for c, learning_rates in configs.items():
+            if dictionnary.value[c]:
+                train_experiment(c, learning_rates)
+    return c, learning_rates, train_experiment
 
 
 @app.cell
-def _(Path, __file__, mo, pd, yaml):
+def _(Path, __file__, configs, mo, pd, yaml):
     import altair as alt
 
     config = "debug"
@@ -63,29 +82,38 @@ def _(Path, __file__, mo, pd, yaml):
     results = []
     training_logs = []
 
-    for d in path.glob(f"{config}_*"):
-        with open(d / "hparams.yaml") as f:
-            hparams = yaml.safe_load(f)
-
-        metrics = pd.read_csv(d / "metrics.csv")
-        results.append({
-            "learning_rate": hparams["learning_rate"],
-            "loss": metrics.iloc[-50]["train_value_loss"].mean()
-        })
-
-        metrics["learning_rate"] = hparams["learning_rate"]
-        training_logs.append(metrics)
+    for config in configs.keys():
+        for d in path.glob(f"{config}_*"):
+            with open(d / "hparams.yaml") as f:
+                hparams = yaml.safe_load(f)
+        
+            metrics = pd.read_csv(d / "metrics.csv")
+            results.append({
+                "config": config,
+                "learning_rate": hparams["learning_rate"],
+                "loss": metrics.iloc[-50]["train_value_loss"].mean()
+            })
+        
+            metrics["config"] = config
+            metrics["learning_rate"] = hparams["learning_rate"]
+            training_logs.append(metrics)
 
     df = pd.DataFrame(results)
 
     chart = alt.Chart(df).mark_point().encode(
         x=alt.X("learning_rate").scale(type="log", nice=False, padding=20),
         y=alt.Y("loss").scale(zero=False, padding=20),
+        color="config"
     ).properties(
         height=500
     )
 
-    loess = chart.transform_loess("learning_rate", "loss").mark_line()
+    loess = chart.transform_loess(
+        "learning_rate",
+        "loss",
+        groupby=["config"],
+        bandwidth=0.3
+    ).mark_line()
 
     mo.ui.altair_chart(chart + loess)
     return (
@@ -107,21 +135,25 @@ def _(Path, __file__, mo, pd, yaml):
 @app.cell
 def _(alt, mo, pd, training_logs):
     df2 = pd.concat(training_logs, ignore_index=True)
-    df2['train_value_loss'] = df2.groupby('learning_rate')['train_value_loss'] \
+    df2["train_value_loss"] = df2.groupby(["config", "learning_rate"])["train_value_loss"] \
         .transform(lambda x: x.rolling(50, center=True, closed="both").mean())
     df2 = df2.dropna(subset=["train_value_loss"])
 
-    loss_chart = alt.Chart(df2).mark_line().encode(
-        x="step",
-        y=alt.Y("train_value_loss").scale(zero=False, padding=20),
-        color=alt.Color("learning_rate:Q").scale(type="log", scheme="viridis"),
-        tooltip=["learning_rate"]
-    ).properties(
-        height=500
-    )
+    def make_loss_chart(df, config_name: str):
+        return alt.Chart(df[df["config"] == config_name]).mark_line().encode(
+            x="step",
+            y=alt.Y("train_value_loss").scale(zero=False, padding=20),
+            color=alt.Color("learning_rate:Q").scale(type="log", scheme="viridis"),
+            tooltip=["learning_rate"]
+        ).properties(
+            height=500
+        )
 
-    mo.ui.altair_chart(loss_chart)
-    return df2, loss_chart
+    mo.vstack([
+        mo.ui.altair_chart(make_loss_chart(df2, "debug")),
+        mo.ui.altair_chart(make_loss_chart(df2, "pico")),
+    ])
+    return df2, make_loss_chart
 
 
 if __name__ == "__main__":
