c0eb824182bce37448810feffcb269d4e085061b
Maxence Frenette <maxence.frenette@gmail.com>
2025-03-09 15:11:32 -0700

    Tweak autoretry

--- src/training/experiments/tune_autoretry.sh
+++ src/training/experiments/tune_autoretry.sh
@@ -3,5 +3,9 @@
 # Run the script with autoretry
 for i in {1..10}; do
     uv run src/training/experiments/tune.py --num-trials 1000
-    echo $? >> autoretry.log
+
+    # If the script exits successfully, break the loop
+    if [ $? -eq 0 ]; then
+        break
+    fi
 done
