92aadc6fab69c26a24957669b27e211685bc0d05
Maxence Frenette <maxence.frenette@gmail.com>
2025-03-09 21:58:04 -0700

    More trials!

--- src/training/experiments/tune_parallel.sh
+++ src/training/experiments/tune_parallel.sh
@@ -2,4 +2,4 @@
 
 rm -rf tuning_logs
 mkdir -p tuning_logs
-seq 1 100 | parallel -j 4 --progress 'uv run src/training/experiments/tune.py --num-trials 10 > tuning_logs/{}.txt 2>&1'
+seq 1 1000 | parallel -j 4 --progress 'uv run src/training/experiments/tune.py --num-trials 10 > tuning_logs/{}.txt 2>&1'
