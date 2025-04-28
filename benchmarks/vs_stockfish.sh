99ccc078eb44665e974e001de9af3311d28dd1f3
Maxence Frenette <maxence.frenette@gmail.com>
2025-04-27 18:54:28 -0700

    Bump down stockfish difficulty

--- benchmarks/vs_stockfish.sh
+++ benchmarks/vs_stockfish.sh
@@ -6,7 +6,8 @@ source $BASEDIR/../.env
 
 $FASTCHESS_PATH \
     -engine cmd=$BASEDIR/engine.sh name=MyEngine nodes=800 \
-    -engine cmd=$(which stockfish) name=stockfish nodes=100 \
+    -engine cmd=$(which stockfish) name=stockfish nodes=20 \
     -openings file=$OPENING_BOOKS_PATH/8moves_v3.pgn format=pgn \
     -log file=$BASEDIR/vs_stockfish.log \
     -pgnout file=$BASEDIR/vs_stockfish.pgn nodes=true
+    -config discard=true
